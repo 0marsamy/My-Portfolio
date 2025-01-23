4c344d12a13e211610b6de1dc8adf004463c163f
omar <omar.samy480@gmail.com>
2025-01-23 17:22:10 +0200

    improve submit form

--- JS/script.js
+++ JS/script.js
@@ -47,3 +47,12 @@ scrollToTopBtn.onclick = function () {
     behavior: 'smooth'
   });
 };
+document.querySelector('form[name="submit-to-google-sheet"]').addEventListener('submit', function (event) {
+  event.preventDefault(); // Prevent default form submission behavior
+
+  // Optionally, handle form data here (e.g., send data to an API or Google Sheet)
+  console.log("Form submitted");
+
+  // Refresh the page after submission
+  location.reload();
+});
